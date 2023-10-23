b66d08aef04165f2b65bb3d7712dab5c1a5ad602
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-23 21:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", fun);
+function fun(){
+	const message = document.createElement("div");
+	message.innerHTML = "DOM load success";
+	document.body.appendChild(message);
+}
